21340c68434a7f06f3ab158598e71c866a4c3edf
priti995 <pawarpreeti2004@gmail.com>
2026-06-16 13:15:45 +0530

    Implemented First Touch Attribution model using SQL

--- first_touch_attribution.sql
+++ first_touch_attribution.sql
@@ -30,20 +30,4 @@ JOIN (
 ON m.User_ID = ft.User_ID
 AND m.Timestamp = ft.First_Touch_Time;
 
-SELECT
-    m.UTM_Source AS First_Touch_Channel,
-    COUNT(*) AS Converted_Users
-FROM `cleaned_marketing_dataset (1)` m
-JOIN (
-    SELECT
-        User_ID,
-        MIN(Timestamp) AS First_Touch_Time
-    FROM `cleaned_marketing_dataset (1)`
-    GROUP BY User_ID
-) ft
-ON m.User_ID = ft.User_ID
-AND m.Timestamp = ft.First_Touch_Time
-WHERE m.Converted = 1
-GROUP BY m.UTM_Source
-ORDER BY Converted_Users DESC;
 
