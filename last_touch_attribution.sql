df9fb8c21f0bc47b22f858c872a3661e64d73572
priti995 <pawarpreeti2004@gmail.com>
2026-06-18 11:59:47 +0530

    Implemented Last Touch Attribution model using SQL

--- last_touch_attribution.sql
+++ last_touch_attribution.sql
@@ -1,33 +1,59 @@
-use marketing_attribution;
-
-SHOW TABLES;
-DESCRIBE `cleaned_marketing_dataset (1)`;
-
-WITH converted_users AS (
-    SELECT DISTINCT User_ID
-    FROM `cleaned_marketing_dataset (1)`
-    WHERE Converted = 1
-),
-
-last_touch AS (
-    SELECT
-        User_ID,
-        UTM_Source,
-        Timestamp,
-        ROW_NUMBER() OVER (
-            PARTITION BY User_ID
-            ORDER BY Timestamp DESC
-        ) AS rn
-    FROM `cleaned_marketing_dataset (1)`
-    WHERE User_ID IN (
-        SELECT User_ID
-        FROM converted_users
-    )
-)
+USE marketing_attribution;
 
 SELECT
     User_ID,
-    UTM_Source AS last_touch_channel,
-    Timestamp AS last_touch_time
-FROM last_touch
-WHERE rn = 1;
+    MAX(Timestamp) AS Last_Touch_Time
+FROM `cleaned_marketing_dataset (1)`
+GROUP BY User_ID;
+
+SELECT
+    m.User_ID,
+    m.UTM_Source AS Last_Touch_Channel,
+    m.Campaign,
+    m.Converted
+FROM `cleaned_marketing_dataset (1)` m
+JOIN (
+    SELECT
+        User_ID,
+        MAX(Timestamp) AS Last_Touch_Time
+    FROM `cleaned_marketing_dataset (1)`
+    GROUP BY User_ID
+) lt
+ON m.User_ID = lt.User_ID
+AND m.Timestamp = lt.Last_Touch_Time;
+
+#Calculate Conversion Credit by Channel
+SELECT
+    m.UTM_Source AS Last_Touch_Channel,
+    COUNT(*) AS Converted_Users
+FROM `cleaned_marketing_dataset (1)` m
+JOIN (
+    SELECT
+        User_ID,
+        MAX(Timestamp) AS Last_Touch_Time
+    FROM `cleaned_marketing_dataset (1)`
+    GROUP BY User_ID
+) lt
+ON m.User_ID = lt.User_ID
+AND m.Timestamp = lt.Last_Touch_Time
+WHERE m.Converted = 1
+GROUP BY m.UTM_Source
+ORDER BY Converted_Users DESC;
+
+#Revenue Attribution by Last Touch
+SELECT
+    m.UTM_Source AS Last_Touch_Channel,
+    SUM(m.Revenue) AS Attributed_Revenue
+FROM `cleaned_marketing_dataset (1)` m
+JOIN (
+    SELECT
+        User_ID,
+        MAX(Timestamp) AS Last_Touch_Time
+    FROM `cleaned_marketing_dataset (1)`
+    GROUP BY User_ID
+) lt
+ON m.User_ID = lt.User_ID
+AND m.Timestamp = lt.Last_Touch_Time
+WHERE m.Converted = 1
+GROUP BY m.UTM_Source
+ORDER BY Attributed_Revenue DESC;
